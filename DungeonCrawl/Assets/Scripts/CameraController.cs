01ec5734f4a4ea9ca71ef839b2694336b3307efe
Todd Williams (RIT Student) <tkw8341@ad.rit.edu>
2016-02-11 18:24:29 -0500

    :) i submitted scripts from my project.

---
+++ DungeonCrawl/Assets/Scripts/CameraController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+	public bool moveOn;
+
+	// Use this for initialization
+	void Start () {
+		moveOn = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	if (moveOn) {
+			this.transform.position += new Vector3(0,0,1) * Time.fixedDeltaTime;
+		}
+	}
+}
