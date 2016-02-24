92c3e5dfef37b491d8d1a38730c04b804ae08003
Kristen Kanekuni <kxk9106@g.rit.edu>
2016-02-23 23:24:45 -0500

    Added Sword Swinging
    Attaches sword and swings when M is pressed

--- DungeonCrawl/Assets/Scripts/Player.cs
+++ DungeonCrawl/Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using System.Collections;
 [RequireComponent (typeof (PlayerController))]
 
 public class Player : MonoBehaviour {
+	//Sword swinging reference
+	//http://www.code4all.nl/dokuwiki/unity_how_to_swing_a_sword
 	
 	public float moveSpeed = 5;
 	
@@ -12,12 +14,22 @@ public class Player : MonoBehaviour {
 	PlayerController controller;
 
 	public GameObject swordButton;
+	private float swingDuration = 0.2f;
+	private float swingSpeed = 0.4f;
+	
+	private float swingTimer = 0f;
+	private bool swinging = false;
+	private Vector3 startRot;
 
 	protected   void  Start () {
 		controller = GetComponent<PlayerController> ();
 		viewCamera = Camera.main;
 		//swordButton = GameObject.Find ("swordButton");
+		startRot = transform.eulerAngles;
 	}
+
+	
+	// Update is called once per frame
 	
 	void Update () {
 
@@ -41,13 +53,37 @@ public class Player : MonoBehaviour {
 		//weapon input
 		if (Input.GetMouseButton (0)) {
 		}
+
+		if (Input.GetKeyDown (KeyCode.M)) {
+			swinging = true;
+		}
+		
+		if (swinging) {
+			swingTimer += Time.deltaTime;
+			
+			if (swingTimer < (swingDuration / 2)) {
+				transform.eulerAngles = Vector3.Lerp(startRot, new Vector3(0, 0, 1), swingSpeed);
+			}
+			
+			if (swingTimer > (swingDuration / 2)) {
+				transform.eulerAngles = Vector3.Lerp(transform.eulerAngles, startRot, swingSpeed);
+			}
+			
+			if (swingTimer > swingDuration) {
+				swingTimer = 0f;
+				swinging = false;
+			}
+		}
 	}
 
 	void OnCollisionEnter(Collision other){
 		if (other.gameObject.tag == "sword") {
-			Destroy (other.gameObject);
-			Debug.Log ("Destroy");
+			//Destroy (other.gameObject);
+			//Debug.Log ("Destroy");
 			swordButton.GetComponent<Image>().color = Color.red;
+			other.transform.localEulerAngles = new Vector3(0,90,50);
+			other.transform.position =  GameObject.FindGameObjectWithTag("hold").transform.position;
+			other.transform.SetParent(this.transform);
 		} 
 		
 	}
