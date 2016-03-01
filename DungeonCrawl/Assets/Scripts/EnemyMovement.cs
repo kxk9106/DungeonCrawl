9eb3eb217a4c01cbd35acc2abcd1a707108a669d
Thomas <wyzwon@gmail.com>
2016-02-29 19:05:27 -0500

    Added Monster placement to map generation (change map array characters to 'x' to place an enemy object there at spawn.

---
+++ DungeonCrawl/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour {
+    
+    
+    GameManager InstanceOfGameManager;
+
+	// Use this for initialization
+	void Start () {
+
+        
+	
+	}
+
+    void Awake()
+    {
+        InstanceOfGameManager = GameObject.FindGameObjectsWithTag("MainGo")[0].GetComponent<GameManager>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
