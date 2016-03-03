78c37495b90235bb8a21b12f9554bd34b6cf133f
Kristen Kanekuni <kxk9106@g.rit.edu>
2016-03-03 14:35:10 -0500

    Walls Take Damage
    If hit 3 times wall will be destroyed

---
+++ DungeonCrawl/Assets/Scripts/wallDie.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class wallDie : MonoBehaviour {
+	GameObject play;
+	Player plScript;
+	int wallHealth = 3;
+	
+	// Use this for initialization
+	void Start () {
+		plScript = FindObjectOfType (typeof(Player)) as Player;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter(Collider other){
+		if (other.gameObject.tag == "sword" && plScript.StopSwinging() == true) {
+			wallHealth --;
+			Debug.Log (wallHealth);
+			if(wallHealth < 1)
+			{
+				Destroy(this.gameObject);
+				Debug.Log("Destroy called");
+
+			}
+
+		} 
+		Debug.Log (other.gameObject.tag);
+		
+	}
+}
