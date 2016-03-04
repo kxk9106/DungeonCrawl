4519331589b954fb0d0e95c71afa193fa1909220
ToddWilliams459 <tkw8341@rit.edu>
2016-03-03 20:41:41 -0500

    Quality of Life Changes
    Arrows dissapear after certain distance
    Enemies have hp
    Arrow destroyed when hits enemy
    something else i think but i forgot oh well.

--- DungeonCrawl/Assets/Scripts/EnemyHurt.cs
+++ DungeonCrawl/Assets/Scripts/EnemyHurt.cs
@@ -4,24 +4,46 @@ using System.Collections;
 public class EnemyHurt : MonoBehaviour {
 	GameObject play;
 	Player plScript;
+	public int health;
 
 	// Use this for initialization
 	void Start () {
 		plScript = FindObjectOfType (typeof(Player)) as Player;
+		health = 3;
 		//Debug.Log (plScript.swinging);
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-	
+		amIalive ();
 	}
 
 	void OnTriggerEnter(Collider other){
 		if (other.gameObject.tag == "sword" && plScript.StopSwinging() == true) {
-			Destroy(this.gameObject);
+			takeDamage ();
+			//Destroy(this.gameObject);
+			Debug.Log("Destroy called");
+		} 
+
+		if (other.gameObject.tag == "projectile") {
+			takeDamage();
+			Destroy (other.gameObject);
+			//Destroy(this.gameObject);
 			Debug.Log("Destroy called");
 		} 
 		Debug.Log (other.gameObject.tag);
-		
+
 	}
+
+	void takeDamage(){
+		health -= 1;
+	}
+
+	void amIalive(){
+		if (health == 0 || health < 0) {
+			Destroy(this.gameObject);
+		}
+	}
+
+
 }
